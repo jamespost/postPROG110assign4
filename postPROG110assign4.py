e422d2f43a1699f9cac03a3131b417fca949b8a6
jamespost <32137126+jamespost@users.noreply.github.com>
2019-11-07 10:17:42 -0600

    Update postPROG110assign4.py
    brought in method to write noise from in class example

--- postPROG110assign4.py
+++ postPROG110assign4.py
@@ -1,3 +1,30 @@
 #James Post Submission for Assignment 4
 
+
+#import necessary modules
+#import numpy
+import numpy as np
+#import matplotlib
+import matplotlib.pyplot as plt
+#get read and write from scipy's input/output module
+from scipy.io.wavfile import write,read
+
+#1.
 #Generate a numpy array of uniform random noise at a specified sample-rate and duration (say1.5 seconds)
+#create a uniform-distribution noise array, plot it and save it to a wav file
+#create 2756 samples with random values between -1 and 1
+noise = np.random.uniform(-1,1,2756)
+
+
+#plot the noise
+plt.plot(noise)
+#scale the noise to a max of 16 bits (up from -1 to 1)
+noise = noise/np.max(np.abs(noise)) * ((2 << 14) -1)
+#plot the scaled noise
+plt.plot(noise)
+#convert to 16 bit ints
+noise = np.int16(noise)
+#print the values of noise
+print(noise)
+#write the noise to a new wav file
+write("noisy.wav",44100,noise)
